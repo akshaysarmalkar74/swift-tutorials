aaa9a6f9d93af663041e76e7ef4cbb45d96b52e2
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2021-08-02 11:38:28 +0530

    Add Datatypes Content

---
+++ Data_Types_and_Variables/datatypes.swift
@@ -0,0 +1,39 @@
+// Character
+var initialChar: Character
+initialChar = "A"
+print(initialChar)
+
+//Int
+var someNum: Int
+someNum = 43
+print(someNum)
+someNum = -32
+print(someNum)
+
+// String
+var fName = "Akshay"
+var lName = "Sarmalkar"
+print(fName)
+print(lName)
+
+// Float
+var someFloat: Float
+someFloat = 3.13432
+print(someFloat)
+
+// Double
+var someDbl = 3.2111
+var anotherDbl: Double
+anotherDbl = 332.1321
+print(type(of: someDbl))
+print(anotherDbl)
+
+
+// Bool
+var isAvailable = true
+print(isAvailable)
+isAvailable = false
+print(isAvailable)
+
+// Find What Type of Value
+print(type(of: someDbl))
