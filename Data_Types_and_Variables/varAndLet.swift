a67c5afef8ececaab413e13a918c01321fc3a7a8
Akshay Sarmalkar <akshaysarmalkar74@gmail.com>
2021-08-02 11:05:51 +0530

    Add Variables Content

---
+++ Data_Types_and_Variables/varAndLet.swift
@@ -0,0 +1,21 @@
+// Var Keyword
+
+var fName = "Akshay"
+print(fName) // Akshay
+
+fName = "Notorious"
+print(fName) // Notorious
+
+var lName: String
+lName = "Sarmalkar"
+print(lName)
+
+// All in one line
+let petName: String = "Tommy"
+print(petName)
+
+// Let Keyword
+let pi = 3.14
+
+// Error
+// let pi = 3.14 
